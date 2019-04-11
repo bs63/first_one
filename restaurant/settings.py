303afb74756c6feab2db37f2d57900e35c449103
bs63 <dhrampalbaluja@gmail.com>
2019-04-11 13:36:18 +0200

    update settings

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -18,14 +18,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '67oj&192$c!sv^l+p23+5uy@^qj(30a6nn9ko2zd@+&#2zyo8i'
+#SECRET_KEY = '67oj&192$c!sv^l+p23+5uy@^qj(30a6nn9ko2zd@+&#2zyo8i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['myfirstexperience.herokuapp.com']
 
 
 # Application definition
