18f875e6c1f1c5003fd1c699e1e33359dd74c0cb
bs63 <47564355+bs63@users.noreply.github.com>
2019-04-13 21:51:11 +0200

    Solve Secret_Key Issue

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG=(os.environ.get('DEBUG_VALUE') == 'True')
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = '67oj&192$c!sv^l+p23+5uy@^qj(30a6nn9ko2zd@+&#2zyo8i'
+SECRET_KEY = '67oj&192$c!sv^l+p23+5uy@^qj(30a6nn9ko2zd@+&#2zyo8i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
